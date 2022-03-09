1d498196f3ce2384b27c10601dba69a7a9d50c1d
"kim" <"ddongcho@naver.com">
2022-03-09 11:27:38 +0900

    second summit

---
+++ Account.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+contract AccountDemo {
+
+    address public whoDeposited;
+    uint public depositAmt;
+    uint public accountBalance;
+
+    function deposit() public payable {
+        whoDeposited = msg.sender;
+        depositAmt = msg.value;
+        accountBalance = address(this).balance;
+    }
+}
